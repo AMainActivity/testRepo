8f6c7ab6683f3a16fc11131ea6ea8bea3979493e
Mikhail Asylbaev <10ama@mail.ru>
2021-10-18 13:32:43 +0500

    test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showToast("kuku")
+    }
+
+    fun showToast(message: String)
+    {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+
+}
