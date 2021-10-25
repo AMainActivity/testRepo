a76775ae01667a119fbd8638d2c165c7920e5dc6
Mikhail Asylbaev <10ama@mail.ru>
2021-10-25 13:25:43 +0500

    test1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tvMA.text="sdsds"
 
         showToast("kuku")
     }
